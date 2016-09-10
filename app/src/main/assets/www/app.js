f3d1a1012171ecee75d121b5ca901914ba7f3576
theel0ja <ojala.s.elias@gmail.com>
2016-09-10 23:03:11 +0300

    Changed template to framework7-multi-platform

---
+++ app/src/main/assets/www/app.js
@@ -0,0 +1,35 @@
+// Determine theme depending on device
+var isAndroid = Framework7.prototype.device.android === true;
+var isIos = Framework7.prototype.device.ios === true;
+ 
+// Set Template7 global devices flags
+Template7.global = {
+    android: isAndroid,
+    ios: isIos
+};
+ 
+// Define Dom7
+var $$ = Dom7;
+ 
+// Change Through navbar layout to Fixed
+if (isAndroid) {
+    // Change class
+    $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+    // And move Navbar into Page
+    $$('.view .navbar').prependTo('.view .page');
+}
+ 
+// Init App
+var myApp = new Framework7({
+    // Enable Material theme for Android device only
+    material: isAndroid ? true : false,
+    // Enable Template7 pages
+    template7Pages: true
+});
+ 
+// Init View
+var mainView = myApp.addView('.view-main', {
+    // Don't worry about that Material doesn't support it
+    // F7 will just ignore it for Material theme
+    dynamicNavbar: true
+});
